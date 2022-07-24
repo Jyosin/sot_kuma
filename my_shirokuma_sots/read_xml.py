28fd8ca1f80ac96912f5fdf080f835eaea2a8bf9
zhangenzhi <zhangsuiyu657@gmail.com>
2022-07-25 00:50:29 +0900

    te: data on server.

--- my_shirokuma_sots/read_xml.py
+++ my_shirokuma_sots/read_xml.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
         b = get_box(path)
         box_args = ['xtl','ytl','xbr','ybr']
         box = [np.float32(b[index][a]) for a in box_args]
-        draw(image, box,name="test_{}.jpg".format(idx))
+        draw(image, box,name="./test_{}.jpg".format(idx))
     
     # make_dataset(videos)
     # gen_json_labels()
