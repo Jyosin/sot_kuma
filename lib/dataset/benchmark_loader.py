d96572b3d6b10a883711be64b1f12e975fd1d00e
root <enzhi@hokudai.com>
2022-08-13 16:09:22 +0000

    feat: test dataset generate.

--- lib/dataset/benchmark_loader.py
+++ lib/dataset/benchmark_loader.py
@@ -54,8 +54,10 @@ class load_sot_benchmark():
             raise Exception('Not implemented benchmark!')
         
     def load_KUMA(self):
+        import pdb
+        pdb.set_trace()
         info = {}
-        json_path = join(realpath(dirname(__file__)), '../../dataset/polar',  "polar" +'.json')
+        json_path = join(realpath(dirname(__file__)), '../../dataset/kuma',  "test" +'.json')
         videos = json.load(open(json_path,'r'))
         for video in videos:
             pass
@@ -73,8 +75,6 @@ class load_sot_benchmark():
             info[v]['image_files'] = [join(base_path, path_name, 'img', im_f) for im_f in info[v]['image_files']]
             info[v]['gt'] = np.array(info[v]['gt_rect']) - [1, 1, 0, 0]
             info[v]['name'] = v
-        import pdb
-        pdb.set_trace()
         return info
 
     def load_VOT(self):
